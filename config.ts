4864d504580a53b54096e9efdcf8f35c6a0ea836
unknown <ebenezerafantsao@gmail.com>
2026-07-24 13:50:08 +0000

    Mise a jour au niveau des candidtures

--- config.ts
+++ config.ts
@@ -3,14 +3,14 @@ export default {
   app_local: isLocal,
   app_dev_url: "http://localhost:8000/api",
   // app_dev_url: "http://192.168.10.105:8000/api",
-  // app_prod_url: "https://escen.neostart.tech/api",
+  app_prod_url: "https://escen.neostart.tech/api",
   // app_prod_url: "https://new.iai-togo.tg/api",
 
 
   
   
   app_dev_storage_url: "http://localhost:8000",
-  // app_prod_storage_url: "https://escen.neostart.tech",
+  app_prod_storage_url: "https://escen.neostart.tech",
   //  app_prod_storage_url: "https://new.iai-togo.tg",
 
 
