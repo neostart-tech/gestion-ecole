663cf78441f8b4b12889b54073f9aadb6a0693a0
unknown <ebenezerafantsao@gmail.com>
2026-07-24 15:28:29 +0000

    Mise a jour de la page de inscription finale

--- dist/_nuxt/BF6zZoO0.js
+++
@@ -1,1 +0,0 @@
-import{au as e}from"./BNgVrraw.js";const t=()=>{const s=e("user"),r=e("userLoaded");return{can:u=>!r.value||!s.value?!1:(s.value.permissions||[]).includes(u)}};export{t as u};
--- dist/_nuxt/CgcFJ-Dc.js
+++
@@ -1,1 +0,0 @@
-import{b1 as r}from"./BNgVrraw.js";var s=r();export{s as O};
